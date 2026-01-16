974cacfa368565410810d631d15dc08eb7daebd8
Jay Joshi <joshijayy421@gmail.com>
2026-01-17 02:28:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,18 @@ const bookingsRoutes = require('./routes/bookings');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// CORS - Allow all origins
-app.use(cors());
+// CORS Headers - MUST BE FIRST
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Max-Age', '86400');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
 
 // Parse JSON bodies
 app.use(express.json());
